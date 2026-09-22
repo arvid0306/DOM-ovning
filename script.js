513e863e04ec149e34a86adffaa492a90a8f8b02
arvid0306 <arvid.johansson@webbpartner.se>
2026-09-22 08:05:05 +0200

    klar med uppgiften

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 // JavaScript
 
 // Globala variabler
-// Håller referenser till värdena för de olika input
+
+// Håller referenser till elementen för de olika input
 let inputElem;
+
 // Håller referensen till p-taggen "message"
 let msgElem;
+
 // Innehåller en array med alla fruktnamn
 let fruitNames;
-// Innehåller en variabel med numret på vald frukt
+
+// Innehåller numret på vald frukt
 let fruitNr;
-//
+
+// Kopplar variablen mot aktuell div i html som senare används för att lägga in img inuti
 let selFruitsElem;
 
 // Funktion som körs då hela webbsidan är inladdad, dvs då all HTML-kod är utförd.
-// Initiering av globala variabler samt koppling avfunktioner till knapparna.
+// Initiering av globala variabler samt koppling av funktioner till knapparna.
 function init() {
-    let button1;
-    let button2;
-    let button3;
-
 	inputElem = [];
     inputElem[1] = document.getElementById("input1");
     inputElem[2] = document.getElementById("input2");
@@ -37,16 +38,16 @@ function init() {
 
     selFruitsElem = document.getElementById("selectedFruits");
 
-    button1 = document.getElementById("btn1");
+    const button1 = document.getElementById("btn1");
     button1.addEventListener("click", e=>{
         showFruit();
     })
 
-    button2 = document.getElementById("btn2");
+    const button2 = document.getElementById("btn2");
     button2.addEventListener("click", e =>{
         checkName();
-
-    button3 = document.getElementById("btn3");
+    })
+    const button3 = document.getElementById("btn3");
     button3.addEventListener("click", e =>{
         addFruits();
     })
@@ -54,10 +55,10 @@ function init() {
 window.onload = init; // Se till att init aktiveras då sidan är inladdad
 
 // Funktionen körs när knappen med id "btn1" klickas.
-// Funktionen hämtar in värdet från textrutan med id "input1".
-// Sedan kontrollerar if-satserna så att det är en siffra och inom rätt intervall.
+// Funktionen hämtar in värdet getNr.
+// Sedan kontrollerar if-satserna så att det är en siffra och inom rätt intervall, detta görs med funktionen getNr.
 // Skulle det inte vara det returnas ett felmeddelande i meddelanderutan.
-// Därefter omvandlas siffran till heltal och läggs in efter "fruits" så att rätt bild som har rätt nummer hämtas.
+// Därefter omvandlas siffran till heltal och läggs in efter "fruit" så att rätt bild som har rätt nummer hämtas.
 // Den sista "msgElem.textConent = ""; används för att tömma felmeddelanderutan när funktionen körs utan problem"
 function showFruit(){
     // Håller värdet för det nummer användaren fyller i inuti "input1"
@@ -68,7 +69,7 @@ function showFruit(){
     let fruitImg;
 
     nr = getNr(1, 5);
-    if(getNr === null){
+    if(nr === null){
         return;
     }
 
@@ -81,8 +82,10 @@ function showFruit(){
 
     fruitNr = nr;
 }
-
+// Funktionen körs när btn2 klickas i. Den kollar så att användaren har skrivit in rätt namn på frukten som visas.
+// Sedan meddelar den användaren om namn-textrutan har en likadan sträng som fruktens namn i arrayen. Dvs om användaren har skrivit rätt
 function checkName(){
+    // Håller värdet för strängen i input2
     let name;
 
     name = inputElem[2].value;
@@ -104,8 +107,11 @@ function checkName(){
         return;
     }
 }
-
+// Funktionen kontrollerar så att användaren har skrivit ett giltigt nummer i vald input och
+// returnerar sedan med null och skriver en kommentar om att skriva ett giltigt nummer eller ett värde på det nummer användaren skrivit fast
+// omvandlat till heltal oavsett om användaren skrev decimaltal eller inte.
 function getNr(elemNr, high){
+    // Håller värdet för vald input.
     let nr;
 
     nr = inputElem[elemNr].value;
@@ -117,22 +123,26 @@ function getNr(elemNr, high){
     }
 
     if(nr < 1 || nr > high){
-       msgElem.textContent = "Skriv enbart i nummer mellan 1 till 5";
+       msgElem.textContent = `Skriv enbart i nummer mellan 1 till ${high}`;
 
        return null;
     }
 
     nr = parseInt(nr);
-    inputElem[1].value = nr;
-
     inputElem[elemNr].value = nr;
 
     return nr;
 }
-
+// Funktionen kontrollerar så at värdet är en siffra och sedan lägger till så många bilder som användaren skrivit i siffror i den tredje textrutan (input3).
+// Bilden som läggs till är samma som visas på hemsidan och detta görs genom att funktionen byter ut siffran i namnet på jpg filen mot siffran för den aktuella fruitNr.
+// Funktionen hämtar fruitNr från den globala variabeln med samma namn. Därför kan man inte använda denna funktion utan att ha kört showFruit först för då blir siffran 0 (tilldelat inuti init funktionen) och då avslutas funktionen direkt.
 function addFruits(){
+    // Håller värdet för return från getNr
     let amount;
+    // Håller textsträngen som skapas för att lägga in nya img-taggar i html
     let imgList;
+    // Används för for-loopen
+    let i;
 
     if(fruitNr === 0){
         return;
@@ -142,4 +152,12 @@ function addFruits(){
     if(amount === null){
         return;
     }
+
+    imgList = "";
+
+    for(i = 0; i < amount; i ++){
+        imgList += "<img src='./pics/fruit" + fruitNr + ".jpg' alt='frukt'>";
+    }
+
+    selFruitsElem.innerHTML += imgList;
 }
